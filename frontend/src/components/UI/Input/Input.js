c6c02f50612c8e7a83a92026072a77db510ff2bc
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-09-12 09:53:45 -0700

    partially update color

--- frontend/src/components/UI/Input/Input.js
+++ frontend/src/components/UI/Input/Input.js
@@ -1,22 +1,26 @@
 import React from "react";
-import classes from './Input.module.css'
+import classes from "./Input.module.css";
 
 const Input = (props) => {
   return (
-    <div
-      className={`${classes.control} ${
-        props.isValid === false ? classes.invalid : ""
-      }`}
-    >
-      <label htmlFor={props.id}>{props.label}</label>
-      <input
-        type= {props.type}
-        id={props.id}
-        value={props.value}
-        onChange={props.onChange}
-        onBlur={props.onBlur}
-      />
-    </div>
+    <React.Fragment>
+      <div className = {classes.control}>
+        <label htmlFor={props.id}>{props.label}</label>
+      </div>
+      <div
+        className={`${classes.control} ${
+          props.isValid === false ? classes.invalid : ""
+        }`}
+      >
+        <input
+          type={props.type}
+          id={props.id}
+          value={props.value}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+        />
+      </div>
+    </React.Fragment>
   );
 };
 
