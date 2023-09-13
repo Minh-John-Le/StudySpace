01eaf8fbd67d354e14d79a793ea18eac1fef9a7d
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-09-13 15:03:36 -0700

    Revert "Add room with filter"
    This reverts commit 20ed6bb5a312df1ed21ad5b9d24f8726869fde05.

--- backend/database/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from .views import RoomCardAPI
-
-urlpatterns = [
-    path('room-card/', RoomCardAPI.as_view()),
-
-]
