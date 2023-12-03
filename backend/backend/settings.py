730705f545d651076c56a3afdb2437484c441bae
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-12-02 21:48:07 -0800

    update minor settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@32*ro71!h-cja0)#^2-c+4q(p)$4*q3uv0g)f#$-!7=bv(1&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True # update later
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",]
 
 
 # Application definition
