d8cbc8892e523177063872479a54ce5729ab79e7
Minh John Le <99804254+Minh-John-Le@users.noreply.github.com>
2023-12-04 03:23:37 -0800

    update study bot avatar

--- frontend/src/components/StudyBot/QAThread.js
+++ frontend/src/components/StudyBot/QAThread.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../UI/Card/Card";
 import classes from "./QAThread.module.css";
 import Avatar from "../UI/Avatar/Avatar";
+import { FaRobot } from "react-icons/fa";
 
 const QAThread = (props) => {
   return (
@@ -22,13 +23,11 @@ const QAThread = (props) => {
 
       <br></br>
       <Card className={classes["body"]}>
-        <div className={classes["writer-info"]}>
-          <Avatar
-            displayName={"Study Bot"}
-            includeDisplayName={true}
-            displayNameClassName={classes["avatar__display-name"]}
-          />
+        <div className={classes["study-bot"]}>
+          <FaRobot size={42} />
+          <h2 className={classes["bot-text"]}>Study Bot</h2>
         </div>
+
         <div className={classes["message-content"]}>
           <pre style={{ whiteSpace: "pre-wrap" }}>{props.response}</pre>
         </div>
